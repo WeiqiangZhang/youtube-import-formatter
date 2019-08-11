d4552a62e6aa8681eccfef65fb069e9855642f8a
DECA UTSC <weiqiang.zhang@mail.utoronto.ca>
2019-08-11 16:12:16 -0400

    Updates

---
+++ precache-manifest.c20b30159872091f3dc46b4bc9851fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07b8560a6a6eb7155b80b0c599e6874e",
+    "url": "/youtube-import-formatter/static/media/youtube_logo.07b8560a.png"
+  },
+  {
+    "revision": "25602a98f82022081b4e",
+    "url": "/youtube-import-formatter/static/js/runtime~main.eb7d470b.js"
+  },
+  {
+    "revision": "f0e5575a2e14ce3435a4",
+    "url": "/youtube-import-formatter/static/js/main.b7ba1a16.chunk.js"
+  },
+  {
+    "revision": "957cf92028411cec0de7",
+    "url": "/youtube-import-formatter/static/js/2.6e0922ee.chunk.js"
+  },
+  {
+    "revision": "f0e5575a2e14ce3435a4",
+    "url": "/youtube-import-formatter/static/css/main.b341e328.chunk.css"
+  },
+  {
+    "revision": "db7f64850ee82397eb424e8b71fca9fa",
+    "url": "/youtube-import-formatter/index.html"
+  }
+];
